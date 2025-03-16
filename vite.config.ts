181aa9a307eedde5fca7748a7ca6c3320364dab4
Zeb Piasecki <zeb@zebulon.dev>
2025-03-16 17:50:14 -0400

    chore: update to orange 0.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import orange from "@orange-js/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [orange(), tsconfigPaths()],
+  plugins: [orange(), tsconfigPaths(), tailwindcss()],
   build: {
     minify: true,
   }
