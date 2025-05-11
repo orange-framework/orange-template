22d42de6b0db5d7de5fa772291481143ec59742b
Zeb Piasecki <zeb@zebulon.dev>
2025-05-11 12:36:46 -0400

    chore: bump orange to 0.2.0

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-export default function Home() {
+import { Route } from ".types/routes/_index";
+
+export default function Home({}: Route.ComponentProps) {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center gap-10">
       <h1 className="text-6xl font-bold text-center">Hello World 🍊</h1>
